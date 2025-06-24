55dcca6b272c5ec662a18db1ac4730f37c1cd88c
Meeth Patel <meeth2060@gmail.com>
2025-06-24 15:06:54 +0530

    completed route and made the movie type interface

---
+++ src/app/api/movie/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { NextRequest} from "next/server";
+
+
+
+
+export async function GET (request : NextRequest){
+    const {searchParams} = new URL(request.url);
+    const title = searchParams.get("title");
+    const api = process.env.OMDB_API_KEY;
+   
+    if(!title || typeof title != 'string'){
+        return NextResponse.json({error:'missing or invalid title'},{status:400})
+    }
+    try{
+            const res = await fetch(`https://www.omdbapi.com/?apikey=${api}&t=${title}`)
+            const data = await res.json()
+            return NextResponse.json(data);
+            
+    }catch(err){
+        return NextResponse.json({err:'failed to fetch movie'},{status:500})
+    }
+
+}
---
+++ src/app/types/type.ts
@@ -0,0 +1,10 @@
+export interface MovieType{
+  title: string;
+  year: string;
+  rating: string;
+  poster: string;
+  plot: string;
+  genre: string;
+  imdbID: string;
+
+}
